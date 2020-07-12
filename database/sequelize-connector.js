3ff62393763c9fba5764fe8a4582c73c3f126f6d
Dat Lieu <lieutandat@gmail.com>
2020-07-12 22:37:07 +0700

    remove test connect db when run unit test

--- database/sequelize-connector.js
+++ database/sequelize-connector.js
@@ -3,15 +3,17 @@ const { Sequelize } = require('sequelize');
 const config = require('../configs');
 const sequelize = new Sequelize(config.database);
 
-const connect = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+if (process.env.NODE_ENV !== 'test') {
+    const connect = async () => {
+        try {
+            await sequelize.authenticate();
+            console.log('Connection has been established successfully.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
     }
-}
 
-connect();
+    connect();
+}
 
 module.exports = sequelize
